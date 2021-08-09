50feab37548a92af33fd186dfd751fea12025c7d
pheonixmaster12 <71676359+pheonixmaster12@users.noreply.github.com>
2021-08-09 17:49:31 -0600

    Add files via upload

---
+++ co1.py
@@ -0,0 +1,8 @@
+import csv
+import plotly.express as px
+import pandas as pd
+
+with open("./co.csv") as csv_file:
+    df = csv.DictReader(csv_file)
+    fig = px.scatter(df, x = "Coffee in ml", y = "sleep in hours", color = "week")
+    fig.show()
